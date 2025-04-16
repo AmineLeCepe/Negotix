e2cf60219661e78ca9097b33af967eca7a081c7c
AmineLeCepe <lionannaba@gmail.com>
2025-04-16 11:51:42 +0100

    Add ejs

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
+require('dotenv').config({ path: './config/.env' });
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error('MongoDB URI is not defined in environment variables');
+        }
+        await mongoose.connect(uri);
         console.log('Connected to MongoDB');
     } catch (e) {
         console.error("Error connecting to MongoDB Atlas: ", e);
+        process.exit(1); // Exit the process if database connection fails
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/mongodb');
+const path = require('path');
+
 
 // App config
 const app = express();
 const port = process.env.PORT || 3000;
 connectDB();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontend/views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 // Middlewares
 app.use(express.json());
@@ -14,7 +23,7 @@ app.use(cors());
 
 // API endpoints
 app.get('/', (req, res) => {
-    res.send('API working');
+    res.render('index', { title: 'Home' });
 })
 
 app.listen(port, () => {
