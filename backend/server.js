d9888926fa8af8b03a54e4552ce29e8b1f9c7c27
AmineLeCepe <lionannaba@gmail.com>
2025-04-20 15:48:24 +0100

    Import models to server, add sections with previously ran insertion queries (Check comments)

--- backend/server.js
+++ backend/server.js
@@ -94,8 +94,94 @@ app.get('/listings', (req, res) => {
     res.render('listings', { items: items, title: 'Listings' });
 });
 
-
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
+
+// Run queries manually
+const mongoose = require("mongoose");
+
+const User = require('./models/userModel');
+const Auction = require('./models/auctionModel');
+const Bid = require('./models/bidModel');
+const Chat = require('./models/chatModel');
+const Message = require('./models/messageModel');
+const Wishlist = require('./models/wishlistModel');
+const Review = require('./models/reviewModel');
+const Category = require('./models/categoryModel');
+
+// async function insertAuction() {
+//     try {
+//         Auction.insertMany([
+//             {
+//                 sellerId: new mongoose.Types.ObjectId('68050461ac9df5ecce3c9efe'),
+//                 title: 'Test',
+//                 description: 'Placeholder description',
+//                 categoryId: new mongoose.Types.ObjectId('68050799f736f109eaf6a2a0'),
+//                 creationDate: new Date(),
+//                 startingPrice: 1000,
+//                 latestPrice: 1000,
+//                 endDate: new Date('2025-05-20'),
+//                 isCompleted: false,
+//             },
+//             {
+//                 sellerId: new mongoose.Types.ObjectId('68050461ac9df5ecce3c9efe'),
+//                 title: 'Test',
+//                 description: 'Placeholder description',
+//                 categoryId: new mongoose.Types.ObjectId('68050799f736f109eaf6a2a0'),
+//                 creationDate: new Date(),
+//                 startingPrice: 1000,
+//                 latestPrice: 1000,
+//                 endDate: new Date('2025-05-20'),
+//                 isCompleted: false,
+//             },
+//             {
+//                 sellerId: new mongoose.Types.ObjectId('68050461ac9df5ecce3c9efe'),
+//                 title: 'Test',
+//                 description: 'Placeholder description',
+//                 categoryId: new mongoose.Types.ObjectId('68050799f736f109eaf6a2a0'),
+//                 creationDate: new Date(),
+//                 startingPrice: 1000,
+//                 latestPrice: 1000,
+//                 endDate: new Date('2025-05-20'),
+//                 isCompleted: false,
+//             },
+//             {
+//                 sellerId: new mongoose.Types.ObjectId('68050461ac9df5ecce3c9efe'),
+//                 title: 'Test',
+//                 description: 'Placeholder description',
+//                 categoryId: new mongoose.Types.ObjectId('68050799f736f109eaf6a2a0'),
+//                 creationDate: new Date(),
+//                 startingPrice: 1000,
+//                 latestPrice: 1000,
+//                 endDate: new Date('2025-05-20'),
+//                 isCompleted: false,
+//             }
+//         ])
+//     } catch (e) {
+//         console.error(e);
+//     }
+// }
+//
+// insertAuction();
+//
+// async function insertCategory() {
+//     try {
+//         Category.insertMany([
+//             { name: 'Electronics' },
+//             { name: 'Sport Equipments' },
+//             { name: 'Decor' },
+//             { name: 'Accessories' },
+//             { name: 'Mobile' },
+//             { name: 'Watches' },
+//             { name: 'Clothes' },
+//             { name: 'Furnitures' },
+//             { name: 'Office' },
+//             { name: 'Cosmetics' },
+//         ])
+//     } catch (e) {
+//         console.error(e);
+//     }
+// }
+//
+// insertCategory();
